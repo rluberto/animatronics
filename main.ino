e8943e82cade7e0667fe0876bf5dc35c5b5e85bf
Ray Luberto <rluberto15@gmail.com>
2021-06-28 16:09:19 -0400

    init

---
+++ main.ino
@@ -0,0 +1,34 @@
+/**
+ * @author
+ * @name 
+ */
+
+
+
+//Init Libraries
+
+//Init  Basic I/O
+
+//Init Servos
+
+//Init NeoPixel
+
+void setup(){
+	
+
+
+}
+
+void loop(){
+	
+}
+
+void runShow(){
+    
+    return;
+}
+
+void idleProgram(){
+    
+    return;
+}
